12066e07b0d397ad7d3826dba9e389b4d004bb0a
@rijswij2 <jaden.van.rijswijk@hva.nl>
2023-05-08 17:51:09 +0200

    details in seperate file and imported

--- index.jsx
+++ index.jsx
@@ -1,49 +1,8 @@
 import ImageAnimator from './image_animator.jsx';
+import { mama_im, kaya_im, mama_widget_styles, kaya_widget_styles, im_styles } from './pixelart_details.jsx';
 
-const mamaWidgetStyles = {
-  width: '250px',
-  height: '250px',
-  position: 'fixed',
-  bottom: '16px',
-  left: '-200px',
-};
-
-const kayaWidgetStyles = {
-  width: '160px',
-  height: '160px',
-  position: 'fixed',
-  bottom: '0',
-  left: '-200px',
-};
-
-const imageStyles = {
-  position: 'absolute',
-  left: '0',
-  bottom: '0',
-  width: '100%',
-  height: '100%',
-  transition: 'transform 0.5s ease',
-};
-
-const kaya_im = [
-  'PixelArt/Kaya/Kaya_walk1.png',
-  'PixelArt/Kaya/Kaya_walk2.png',
-  'PixelArt/Kaya/Kaya_walk3.png',
-  'PixelArt/Kaya/Kaya_walk4.png',
-  'PixelArt/Kaya/Kaya_walk5.png',
-];
-
-const mama_im = [
-  'PixelArt/Mama/walk1.png',
-  'PixelArt/Mama/walk2.png',
-  'PixelArt/Mama/walk3.png',
-  'PixelArt/Mama/walk4.png',
-  'PixelArt/Mama/walk5.png',
-  'PixelArt/Mama/walk6.png',
- ];
-
-let mama_animator = new ImageAnimator(mama_im, mamaWidgetStyles, imageStyles, 'mama', 9, 24, 800);
-let kaya_animator = new ImageAnimator(kaya_im, kayaWidgetStyles, imageStyles, 'kaya', 9, 15, 1100);
+let mama_animator = new ImageAnimator(mama_im, mama_widget_styles, im_styles, 'mama', 9, 24, 800);
+let kaya_animator = new ImageAnimator(kaya_im, kaya_widget_styles, im_styles, 'kaya', 9, 15, 1100);
 
 
 mama_animator.start();
@@ -55,11 +14,11 @@ setTimeout(() => {
 
 export const render = () => (
   <div>
-    <div style={mamaWidgetStyles}>
-      <img id="mama" src={mama_im[0]} style={imageStyles} />
+    <div style={mama_widget_styles}>
+      <img id="mama" src={mama_im[0]} style={im_styles} />
     </div>
-    <div style={kayaWidgetStyles}>
-      <img id="kaya" src={kaya_im[0]} style={imageStyles} />
+    <div style={kaya_widget_styles}>
+      <img id="kaya" src={kaya_im[0]} style={im_styles} />
     </div>
   </div>
 );
---
+++ pixelart_details.jsx
@@ -0,0 +1,41 @@
+export const mama_widget_styles = {
+    width: '250px',
+    height: '250px',
+    position: 'fixed',
+    bottom: '16px',
+    left: '-200px',
+  };
+  
+export const kaya_widget_styles = {
+    width: '160px',
+    height: '160px',
+    position: 'fixed',
+    bottom: '0',
+    left: '-200px',
+  };
+  
+export const im_styles = {
+    position: 'absolute',
+    left: '0',
+    bottom: '0',
+    width: '100%',
+    height: '100%',
+    transition: 'transform 0.5s ease',
+  };
+  
+export const kaya_im = [
+    'PixelArt/Kaya/Kaya_walk1.png',
+    'PixelArt/Kaya/Kaya_walk2.png',
+    'PixelArt/Kaya/Kaya_walk3.png',
+    'PixelArt/Kaya/Kaya_walk4.png',
+    'PixelArt/Kaya/Kaya_walk5.png',
+  ];
+  
+export const mama_im = [
+    'PixelArt/Mama/walk1.png',
+    'PixelArt/Mama/walk2.png',
+    'PixelArt/Mama/walk3.png',
+    'PixelArt/Mama/walk4.png',
+    'PixelArt/Mama/walk5.png',
+    'PixelArt/Mama/walk6.png',
+   ];
